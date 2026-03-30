df179c5af671154c969ba18b628083bde47f3359
Ifaz Md Zahin <zahin26112004@gmail.com>
2026-03-30 23:15:07 +0600

    Update some module-info.java for better management

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,17 @@ module com.example.music_band_oop {
     requires javafx.controls;
     requires javafx.fxml;
 
-    // Export the main package
+    // Main package
     opens com.example.music_band_oop to javafx.fxml;
     exports com.example.music_band_oop;
 
+
     opens com.example.music_band_oop.Controller to javafx.fxml;
     exports com.example.music_band_oop.Controller;
+
+    opens com.example.music_band_oop.Controller.FXMLControllerForUser1 to javafx.fxml;
+    exports com.example.music_band_oop.Controller.FXMLControllerForUser1;
+
+    opens com.example.music_band_oop.Controller.FXMLControllerForUser2 to javafx.fxml;
+    exports com.example.music_band_oop.Controller.FXMLControllerForUser2;
 }
