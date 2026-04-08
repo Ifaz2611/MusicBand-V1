631ab13071bbfe812f06e89a534360649792ce96
anonnokhan <anonnokhan94@gmail.com>
2026-04-08 12:11:13 +0600

    done

---
+++ src/main/java/com/example/music_band_oop/nasim/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.music_band_oop.nasim;
+
+public class DemoController
+{
+    @javafx.fxml.FXML
+    private TextField fxid;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void abc(ActionEvent actionEvent) {
+    }
+}
